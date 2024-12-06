cc317dadf821579de4365d01168f4d6ea3a8b24a
Juan Oliva <olivajuan12@gmail.com>
2024-12-06 15:40:55 -0300

    jsjs initial commit

---
+++ go.mod
@@ -0,0 +1,15 @@
+module jsjs
+
+go 1.22.1
+
+require (
+	github.com/BishopFox/jsluice v0.0.0-20240110145140-0ddfab153e06 // indirect
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/ditashi/jsbeautifier-go v0.0.0-20141206144643-2520a8026a9c // indirect
+	github.com/juanmoliva/ureal v0.0.0-20241202133651-0244b133bdae // indirect
+	github.com/smacker/go-tree-sitter v0.0.0-20230720070738-0d0a9f78d8f8 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/net v0.15.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,157 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/BishopFox/jsluice"
+	"github.com/juanmoliva/ureal/pkg/requests"
+	flag "github.com/spf13/pflag"
+)
+
+func main() {
+	var Threads int
+	var DebugMode bool
+	var Proxy string
+
+	flag.IntVarP(&Threads, "threads", "t", 5, "number of threads, default 5")
+	flag.BoolVarP(&DebugMode, "debug", "d", false, "enable debug mode")
+	flag.StringVar(&Proxy, "proxy", "", "proxy URL")
+	flag.Parse()
+
+	// spin up an output worker
+	output := make(chan string)
+	errs := make(chan error)
+	done := make(chan any)
+
+	go func() {
+
+		for {
+			select {
+			case out := <-output:
+				if out == "" {
+					continue
+				}
+				fmt.Println(out)
+			case err := <-errs:
+				fmt.Fprintf(os.Stderr, "error: %s\n", err)
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	jobs := make(chan string)
+
+	httpClientConfig := requests.HttpClientConfig{
+		DebugMode: DebugMode,
+		Proxy:     Proxy,
+	}
+
+	var httpClients []requests.HttpClient
+	for i := 0; i < Threads; i++ {
+		httpClients = append(httpClients, requests.NewClient(httpClientConfig))
+	}
+
+	var wg sync.WaitGroup
+	for i := 0; i < Threads; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			for url := range jobs {
+				source, err := getSource(url, &httpClients[i])
+
+				if err != nil {
+					errs <- err
+					continue
+				}
+
+				extractURLsAndSecrets(url, source, output, errs)
+			}
+		}(i)
+	}
+
+	var r io.Reader = os.Stdin
+	input := bufio.NewScanner(r)
+
+	for input.Scan() {
+		url := input.Text()
+
+		if strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://") {
+			jobs <- url
+		}
+	}
+	close(jobs)
+
+	wg.Wait()
+	done <- struct{}{}
+	close(output)
+	close(errs)
+
+}
+
+func getSource(url string, client *requests.HttpClient) ([]byte, error) {
+
+	httpReqConfig := requests.HttpReqConfig{
+		HTTPMethod: requests.GET,
+	}
+
+	httpResp, err := client.Make(url, httpReqConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return httpResp.Body, nil
+}
+
+var patternsJson = "[   {     \"name\": \"base64\",     \"value\": \"(eyJ|YTo|Tzo|PD[89]|rO0)[%a-zA-Z0-9+/]+={0,2}\",     \"severity\": \"low\"   },   {     \"name\": \"genericSecret\",     \"key\": \"(secret|private|apiKey|token|api_key|apikey)\",     \"value\": \"[%a-zA-Z0-9+/]+\"   },  {    \"name\": \"firebaseConfig\",    \"severity\": \"high\",    \"object\": [      {\"key\": \"apiKey\", \"value\": \"^AIza.+\"},      {\"key\": \"authDomain\"},      {\"key\": \"projectId\"},      {\"key\": \"storageBucket\"}    ]  }]"
+
+func extractURLsAndSecrets(filename string, source []byte, output chan string, errs chan error) {
+	seen := make(map[string]any, 0)
+
+	analyzer := jsluice.NewAnalyzer(source)
+
+	// parse patterns
+	patternsReader := strings.NewReader(patternsJson)
+	patterns, err := jsluice.ParseUserPatterns(patternsReader)
+	if err != nil {
+		errs <- err
+		return
+	}
+
+	analyzer.AddSecretMatchers(patterns.SecretMatchers())
+
+	matches := analyzer.GetSecrets()
+	for _, match := range matches {
+
+		match.Filename = filename
+
+		j, err := json.Marshal(match)
+		if err != nil {
+			continue
+		}
+		output <- string(j)
+	}
+
+	for _, m := range analyzer.GetURLs() {
+		m.Filename = filename
+
+		if _, exists := seen[m.URL]; exists {
+			continue
+		}
+
+		seen[m.URL] = struct{}{}
+
+		j, err := json.Marshal(m)
+		if err != nil {
+			errs <- err
+			continue
+		}
+		output <- string(j)
+	}
+}
